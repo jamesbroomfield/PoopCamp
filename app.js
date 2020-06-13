276b4729bc601b9aeb5b9a0dc5a14aceac1d9a6d
James Broomfield <jbroomfield2000@gmail.com>
2020-06-13 20:02:15 +0100

    update app.js to connect to mongodb using environment variable

--- app.js
+++ app.js
@@ -51,12 +51,15 @@ app.use(function(req, res, next){
 	next();
 });
 
+//Connect to MongoDB database using environment variable - see following for specifics
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
 
 //Connect to MongoDB datatbase - first argument is location of DB, second just gets rid of some deprecation warnings
 //mongoose.connect("mongodb://localhost/poop_camp", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 //Connection for deployed DB
-mongoose.connect("mongodb+srv://jamesb:Overcr0ak!@cluster0-hpmow.mongodb.net/poop_camp?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+//mongoose.connect("mongodb+srv://jamesb:Overcr0ak!@cluster0-hpmow.mongodb.net/poop_camp?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 //Tells the app to use bodyParser to deal with form submissions
 app.use(bodyParser.urlencoded({extended: true}));
